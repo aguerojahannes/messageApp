2a4e6faec6b48e16fe3d95d07804943ca7fa0e12
ajahannes <aguerojahannes@gmail.com>
2015-10-08 17:23:25 -0700

    added array of names

--- public/javascript/controllers/AddMessageController.js
+++ public/javascript/controllers/AddMessageController.js
@@ -1,7 +1,20 @@
-angular.module('MessageApp').controller('AddMessageController', function($http, $state, MessageFactory) {
+   angular.module('MessageApp').controller('AddMessageController', function($http, $state, MessageFactory) {
    var vm = this;
    vm.messages = {};
 
+vm.group =[
+   { name: "Pearl",
+   image: "https://material.angularjs.org/0.11.2/img/washedout.png",
+   color:""
+},
+{ name: "Yisel",
+   image: "",
+   color:""},
+
+   { name: "Ana",
+   image: "",
+   color:""}
+]
    vm.addMessage = function(){
       MessageFactory.addMessage(vm.messages);
    console.log("YAYYY!");
